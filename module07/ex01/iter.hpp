385c3fc0c1309c988ec2575bd91ef925eb73a801
Ruben Pinchasov <rpinchas@c3r4p2.42vienna.com>
2024-10-24 13:54:26 +0200

    correct iter

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -3,10 +3,12 @@
 #include <cstddef>
 #include <string>
 
-template<typename T>
-void iter(T *array, size_t len, void (*func)(T)) {
+//also possible to send void (*func)(T&) but 
+//then there need to be (const T&) to cover const types.
+template<typename T, typename F>
+void iter(T *array, size_t len, F funcptr) {
 	for (size_t i = 0; i < len; i++)
-		func(array[i]);
+		funcptr(array[i]);
 }
 
 #endif
